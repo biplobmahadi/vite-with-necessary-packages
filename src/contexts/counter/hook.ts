6cf4fcff1d637f904b175d47638dda0f001a1f6d
Biplob <biplobhasan547@gmail.com>
2022-06-08 18:59:20 +0600

    create context hook

---
+++ src/contexts/counter/hook.ts
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { CounterContext } from "./provider";
+
+export const useCounterContext = () => {
+  const context = useContext(CounterContext);
+  if (context === undefined) {
+    throw new Error("useCounterContext must be used within a CounterProvider");
+  }
+  const { count, increment, decrement, reset } = context;
+  return { count, increment, decrement, reset };
+};
