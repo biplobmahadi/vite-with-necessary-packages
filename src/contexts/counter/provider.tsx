190766e2a007525ccc56c20e1eecdb3b5d25ae91
Biplob <biplobhasan547@gmail.com>
2022-06-08 18:58:26 +0600

    create counter context provider

---
+++ src/contexts/counter/provider.tsx
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+import IChildrenComponent from "../../interfaces/common/IChildrenComponent";
+import { ICounterContextValue } from "../../interfaces/counter";
+import createActions from "./actionCreator";
+import initialState from "./initialState";
+import counterReducer from "./reducer";
+
+export const CounterContext = createContext<ICounterContextValue | undefined>(
+  undefined
+);
+
+const CounterContextProvider = ({ children }: IChildrenComponent) => {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+  const { count } = state;
+  const { ...actions } = createActions(dispatch);
+
+  return (
+    <CounterContext.Provider value={{ count, ...actions }}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
+
+export default CounterContextProvider;
