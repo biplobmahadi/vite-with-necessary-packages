cd8a768ea5783ccb3b28fc539dbacc35e103cfb5
Biplob <biplobhasan547@gmail.com>
2022-06-10 18:34:29 +0600

    render all on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Posts from "./components/Posts";
+import Practice from "./components/Practice";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,7 +9,10 @@ function App() {
     <div>
       <button type="button" onClick={() => setCount((count) => count + 1)}>
         count is: {count}
-      </button>
+      </button>{" "}
+      {count === 1 ? <Posts /> : null}
+      {/* <Posts />
+      <RenderHoi /> */}
     </div>
   );
 }
