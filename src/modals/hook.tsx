de2862c18f1218b7a46eac26d91591269f21020d
Biplob <biplobhasan547@gmail.com>
2022-06-22 17:33:44 +0600

    create modal hook

---
+++ src/modals/hook.tsx
@@ -0,0 +1,29 @@
+import { FC, useState } from "react";
+import { IChildren } from "../interfaces/common";
+import BaseModal from "./BaseModal";
+
+// in parent component which use this hook
+// if hook value change parent component will rerender
+// so use wisely
+
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  const RenderedModal: FC<IChildren> = ({ children }) => {
+    if (isOpen) {
+      return (
+        <BaseModal isOpen={isOpen} close={close}>
+          {children}
+        </BaseModal>
+      );
+    }
+    return null;
+  };
+
+  return { isOpen, open, close, RenderedModal };
+};
+
+export default useModal;
